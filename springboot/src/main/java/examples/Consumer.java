1d1d684c2cc8b335bb74151c28502ee0dbc175fc
Yeva Byzek <yeva@confluent.io>
2021-08-27 15:26:29 -0400

    Delete comment

--- springboot/src/main/java/examples/Consumer.java
+++ springboot/src/main/java/examples/Consumer.java
@@ -19,7 +19,6 @@ public class Consumer {
     public void listen(String value,
         @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
         @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key) {
-        //logger.info(String.format("Consumed event from topic %s: key = %-10s value = %s", record.topic(), record.key(), record.value()));
         logger.info(String.format("Consumed event from topic %s: key = %-10s value = %s", topic, key, value));
     }
 }
