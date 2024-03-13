6fc640dae04b95396fe29c37dc355091ca2b3a40
Bill Bejeck <bbejeck@gmail.com>
2024-03-13 15:44:23 -0400

    Update Spring Boot and Java

--- spring-boot/src/main/java/examples/Producer.java
+++ spring-boot/src/main/java/examples/Producer.java
@@ -1,13 +1,16 @@
 package examples;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.kafka.support.ProducerListener;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class Producer {
@@ -19,17 +22,16 @@ public class Producer {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String key, String value) {
-            ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(TOPIC, key, value);
-             future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
-                 @Override
-                 public void onSuccess(SendResult<String, String> result) {
-                     logger.info(String.format("Produced event to topic %s: key = %-10s value = %s", TOPIC, key, value));
-                 }
-                 @Override
-                 public void onFailure(Throwable ex) {
-                     ex.printStackTrace();
-                 }
-             });
+           CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(TOPIC, key, value);
+
+           future.whenComplete((result, ex) -> {
+               if (ex == null) {
+                   logger.info(String.format("Produced event to topic %s: key = %-10s value = %s", result.getRecordMetadata().topic(), key, value));
+               } else {
+                   ex.printStackTrace(System.out);
+               }
+           });
+             
     }
 
 }
