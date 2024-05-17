1f0724faf3e94d29b587822670b456c1a83946a5
Dave Troiano <dtroiano@confluent.io>
2024-05-17 12:56:25 -0400

    inline properties in Java guide

---
+++ java/src/main/java/io/confluent/developer/ConsumerExampleCloudBasic.java
@@ -0,0 +1,53 @@
+package io.confluent.developer;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.CommonClientConfigs.SECURITY_PROTOCOL_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+import static org.apache.kafka.common.config.SaslConfigs.SASL_MECHANISM;
+
+public class ConsumerExample {
+
+    public static void main(final String[] args) {
+
+        final Properties props = new Properties() {{
+            // User-specific properties that you must set
+            put(BOOTSTRAP_SERVERS_CONFIG, "<BOOTSTRAP SERVERS>");
+            put("sasl.username", "<CLUSTER API KEY>");
+            put("sasl.password", "<CLUSTER API SECRET>");
+
+            // Fixed properties
+            put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
+            put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
+            put(GROUP_ID_CONFIG, "kafka-java-getting-started");
+            put(AUTO_OFFSET_RESET_CONFIG, "earliest");
+            put(SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+            put(SASL_MECHANISM, "PLAIN");
+        }};
+
+        final String topic = "purchases";
+
+        try (final Consumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(Arrays.asList(topic));
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    String key = record.key();
+                    String value = record.value();
+                    System.out.println(
+                            String.format("Consumed event from topic %s: key = %-10s value = %s", topic, key, value));
+                }
+            }
+        }
+    }
+
+}
---
+++ java/src/main/java/io/confluent/developer/ProducerExampleLocal.java
@@ -0,0 +1,50 @@
+package io.confluent.developer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.Random;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+public class ProducerExample {
+
+    public static void main(final String[] args) {
+        final Properties props = new Properties() {{
+            // User-specific properties that you must set
+            put(BOOTSTRAP_SERVERS_CONFIG, "localhost:<PLAINTEXT PORTS>");
+
+            // Fixed properties
+            put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
+            put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
+            put(ACKS_CONFIG, "all");
+        }};
+
+        final String topic = "purchases";
+
+        String[] users = {"eabara", "jsmith", "sgarcia", "jbernard", "htanaka", "awalther"};
+        String[] items = {"book", "alarm clock", "t-shirts", "gift card", "batteries"};
+        try (final Producer<String, String> producer = new KafkaProducer<>(props)) {
+            final Random rnd = new Random();
+            final int numMessages = 10;
+            for (int i = 0; i < numMessages; i++) {
+                String user = users[rnd.nextInt(users.length)];
+                String item = items[rnd.nextInt(items.length)];
+
+                producer.send(
+                        new ProducerRecord<>(topic, user, item),
+                        (event, ex) -> {
+                            if (ex != null)
+                                ex.printStackTrace();
+                            else
+                                System.out.printf("Produced event to topic %s: key = %-10s value = %s%n", topic, user, item);
+                        });
+            }
+            System.out.printf("%s events were produced to topic %s%n", numMessages, topic);
+        }
+
+    }
+}
